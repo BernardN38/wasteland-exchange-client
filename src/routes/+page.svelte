e9ba17199e66251434c91b97ab3b5bcb0e509fdb
bernardn <erisboxx@gmail.com>
2026-01-17 22:59:00 -0600

    add basic navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+<nav style="display:flex; gap: 12px; padding: 12px 0;">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
