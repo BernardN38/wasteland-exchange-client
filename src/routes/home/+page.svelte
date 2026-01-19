c8847c5966c9d37c1e6732424ac6d255db56743a
bernardn <erisboxx@gmail.com>
2026-01-18 19:42:32 -0600

    remove unused boilerplate code

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-    fetch("https://dev.bernardn.com/api/health").then((res) => {
-        if (!res.ok) {
-            console.error("API health check failed");
-        } else {
-            console.log(res.body);
-        }
-    });
 </script>
 
 <h1>Home</h1>
